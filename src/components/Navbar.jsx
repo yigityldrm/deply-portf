2bcf021767c94119dd728efa42f8d07c36da25e4
Yiğit Yıldırım <92275149+yigityldrm@users.noreply.github.com>
2024-03-04 11:45:25 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -103,7 +103,7 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='https://github.com/b2200356091'
+              href='https://github.com/yigityldrm'
             >
               Github <FaGithub size={30} />
             </a>
